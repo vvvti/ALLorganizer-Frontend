a240fbad8b499ee7eb0d7487084d9e5b53274ba8
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-12-17 23:32:05 +0100

    ALL-23 - Refactor HomeCards component and add styles

---
+++ src/components/HomeCards/HomeCards.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+     max-width: 90%;
+     height: 85vh;
+     margin: auto;
+     display: flex;
+     flex-direction: row;
+     align-items: center;
+     justify-content: center;
+`;
--- src/components/HomeCards/HomeCards.tsx
+++ src/components/HomeCards/HomeCards.tsx
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import {StyledContainer} from './HomeCards.styled';
 
 const useStyles = makeStyles({
   root: {
@@ -21,6 +22,7 @@ export const HomeCards = () => {
   const classes = useStyles();
 
   return (
+    <StyledContainer>
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
@@ -46,5 +48,56 @@ export const HomeCards = () => {
         </Button>
       </CardActions>
     </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            ALLorganizer
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nam asperiores sapiente id odit eligendi reiciendis voluptatem praesentium atque officia?
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            ALLorganizer
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nam asperiores sapiente id odit eligendi reiciendis voluptatem praesentium atque officia?
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+    </StyledContainer>
   );
 }
