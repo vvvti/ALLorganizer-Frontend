c7693975acaadf0396aafa6c35dfbc93f8ec5961
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2021-01-01 23:09:53 +0100

    ALL-24 - Add styles for Daschboard Grid component

--- src/components/DaschboardGrid/DashboardGrid.styled.ts
+++ src/components/DaschboardGrid/DashboardGrid.styled.ts
@@ -2,6 +2,23 @@ import styled from 'styled-components';
 
 
 export const StyledContainer = styled.div`
-    padding: 10px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 100px 1fr;
+    align-items: flex-start;
+    grid-gap: 10px;
+    grid-template-areas:
+        'cbc cbc ast ast'
+        'crp crp analyses analyses';
+    min-height: 90vh;
+    margin: 20px 0;
 `;
 
+export const StyledCbcContainer = styled.div`
+    grid-area: profile;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0 4px 12px;
+    min-height: 100%;
+`;
