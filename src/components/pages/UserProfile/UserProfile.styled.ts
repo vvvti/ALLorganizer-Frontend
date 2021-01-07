75dd5cdd498e8ef23d90647ff2003011b23984b5
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2021-01-07 20:22:30 +0100

    ALL-24 - Refactor UserProfile component and styles

--- src/components/pages/UserProfile/UserProfile.styled.ts
+++ src/components/pages/UserProfile/UserProfile.styled.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
+import {Avatar} from '@material-ui/core';
 
 export const StyledPageContainer = styled.div`
-    margin: 0;
+    display: grid;
 `;
+
+export const StyledProfileContainer = styled.div`
+    margin-top: 8em;
+`;
+
+export const StyledAvatar = styled(Avatar)`
+    align-self: center;
+    justify-self: center;
+    width: 8em;
+    height: 8em;
+    margin-bottom: 20px;
+`; 
