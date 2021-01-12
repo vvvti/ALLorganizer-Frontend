417a3f1fa146726b84f673083670b377f78eda0c
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2021-01-12 23:07:06 +0100

    ALL-24 - Add buttons and styles in DaschboardDrawer component

--- src/components/DaschboardGrid/DashboardGrid.tsx
+++ src/components/DaschboardGrid/DashboardGrid.tsx
@@ -1,17 +1,53 @@
 import React from 'react'
-import {StyledContainer, StyledCbcContainer, StyledCrpContainer, StyledAstContainer, StyledUrineContainer} from './DashboardGrid.styled';
+import {
+    StyledContainer,
+    StyledCbcContainer,
+    StyledCrpContainer,
+    StyledAstContainer,
+    StyledUrineContainer,
+    StyledButton
+} from './DashboardGrid.styled';
 
 export const DashboardGrid: React.FC = () => {
     return (
         <StyledContainer>
             
             <StyledCbcContainer>
-            <h5>Morfologia</h5>
-            </StyledCbcContainer>
-            
-            <StyledAstContainer><h5>Morfologia</h5></StyledAstContainer>
-            <StyledCrpContainer><h5>Morfologia</h5></StyledCrpContainer>
-            <StyledUrineContainer><h5>Morfologia</h5></StyledUrineContainer>
+                <h5>Morfologia</h5>
+                    <StyledButton
+                        size="small"
+                        variant="contained"
+                        color="primary">
+                            Dodaj wyniki badań
+                    </StyledButton>
+            </StyledCbcContainer>            
+            <StyledAstContainer>
+                <h5>Biochemia</h5>
+                <StyledButton
+                        size="small"
+                        variant="contained"
+                        color="primary">
+                            Dodaj wyniki badań
+                </StyledButton>
+            </StyledAstContainer>
+            <StyledCrpContainer>
+                <h5>CRP</h5>
+                <StyledButton
+                        size="small"
+                        variant="contained"
+                        color="primary">
+                            Dodaj wyniki badań
+                </StyledButton>
+            </StyledCrpContainer>
+            <StyledUrineContainer>
+                <h5>Badania moczu</h5>
+                <StyledButton
+                        size="small"
+                        variant="contained"
+                        color="primary">
+                            Dodaj wyniki badań
+                </StyledButton>
+            </StyledUrineContainer>
         </StyledContainer>
     )
 }
