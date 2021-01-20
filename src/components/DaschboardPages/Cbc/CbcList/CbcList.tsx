452b7f902e8b5e39770b3f9e55ef09f5c9ad65d4
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2021-01-20 23:05:09 +0100

    ALL-25 - Create CbcList component

---
+++ src/components/DaschboardPages/Cbc/CbcList/CbcList.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {ColDef, DataGrid} from '@material-ui/data-grid';
+import {StyledButton, StyledContainer} from './CbcList.styled';
+
+export const CbcList: React.FC = () => {
+
+
+    // const rowsData = CbcList.map((obj: any) => ({
+    //     ...obj,
+    //     finalValue: obj.calculation.finalValue.toFixed(2),
+    //     periodValue: obj.calculation.periodValue,
+    //     idValue: obj.id.substring(0, 8),
+    // }));
+
+    const columns: ColDef[] = [
+        {field: 'idValue', headerName: 'Received Offers', width: 180},
+        {field: 'username', headerName: 'Username', type: 'string', width: 110},
+        {field: 'interestRate', headerName: 'Return (%)', type: 'number', width: 150},
+        {field: 'offerAmount', headerName: 'Amount (GBP)', type: 'number', width: 150},
+        {field: 'finalValue', headerName: 'Final Value(GBP)', type: 'number', width: 200},
+        {
+            field: '',
+            headerName: 'Actions',
+            sortable: false,
+            width: 135,
+            disableClickEventBubbling: true,
+            renderCell: () => {
+                return (
+                    <StyledButton type="submit" size="small" variant="contained" color="primary">
+                        Accept
+                    </StyledButton>
+                );
+            },
+        },
+    ];
+
+    const rows = [
+        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+      ];
+
+    return (
+        <StyledContainer>
+            <DataGrid rows={rows} columns={columns} pageSize={9} />
+        </StyledContainer>
+    );
+};
