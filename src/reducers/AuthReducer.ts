81e75f6c87394f630948a66bf9da80c9f2e52d51
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2021-02-02 17:00:39 +0100

    ALL-26 - Add redux, redux-thunk, and types for it for global state management
    create store, action types and reducers for authorization

---
+++ src/reducers/AuthReducer.ts
@@ -0,0 +1,66 @@
+import {
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+  } from "../actions/types";
+
+interface InitialStateI {
+  token: string | null,
+  isAuthenticated: boolean | null,
+  isLoading: boolean,
+  user: boolean |null
+}
+
+const initialState: InitialStateI = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    isLoading: false,
+    user: null
+};
+
+export const authReducer = (state: InitialStateI = initialState, action: any): InitialStateI => {
+    switch (action.type) {
+        case USER_LOADING:
+          return {
+            ...state,
+            isLoading: true
+          };
+        case USER_LOADED:
+          return {
+            ...state,
+            isAuthenticated: true,
+            isLoading: false,
+            user: action.payload
+          };
+    
+        case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS:
+          localStorage.setItem("token", action.payload.token);
+          return {
+            ...state,
+            ...action.payload,
+            isAuthenticated: true,
+            isLoading: false
+          };
+    
+        case AUTH_ERROR:
+        case LOGIN_FAIL:
+        case LOGOUT_SUCCESS:
+        case REGISTER_FAIL:
+          return {
+            ...state,
+            token: null,
+            user: null,
+            isAuthenticated: false,
+            isLoading: false
+          };
+    
+        default:
+          return state;
+      }
+}
