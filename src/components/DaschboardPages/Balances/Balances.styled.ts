9e4224d10316f948090bc9c3d1990a0d21091238
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2021-02-15 23:32:19 +0100

    ALL-28 - Implement grid i Balances component

---
+++ src/components/DaschboardPages/Balances/Balances.styled.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import {colors} from '../../../styles/theme';
+
+
+export const StyledContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    align-items: flex-start;
+    grid-gap: 10px;
+    grid-template-areas:
+        'all all first first'
+        'all all first first'
+        'all all first first';
+    margin: 20px 0;
+`;
+
+export const StyledAllContainer = styled.div`
+    grid-area: all;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0 4px 12px ${colors.boxShadow};
+    height: 72vh;
+`;
+
+export const StyledFirstContainer = styled.div`
+    grid-area: first;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0 4px 12px ${colors.boxShadow};
+    height: 72vh;
+`;
