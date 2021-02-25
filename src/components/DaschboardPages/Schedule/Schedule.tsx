b7024555ad07151722b792578488d2e8818f7faf
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2021-02-25 22:30:22 +0100

    ALL-29 - Add grid to Schedule component

--- src/components/DaschboardPages/Schedule/Schedule.tsx
+++ src/components/DaschboardPages/Schedule/Schedule.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {StyledContainer, StyledFirstContainer, StyledSecondContainer, StyledThirdContainer} from './Schedule.styled';
 
 export const Schedule: React.FC = () => {
     return (
-        <div>
-           <h1>Schedule</h1> 
-        </div>
+        <StyledContainer>            
+            <StyledFirstContainer><h4>First</h4></StyledFirstContainer>
+            <StyledSecondContainer><h4>Second</h4></StyledSecondContainer>
+            <StyledThirdContainer><h4>Third</h4></StyledThirdContainer>
+        </StyledContainer>
     )
 }
 
