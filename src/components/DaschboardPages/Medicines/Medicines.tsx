4594548deff751e4cf103b6cba82603aa6f97323
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2021-03-01 23:33:18 +0100

    ALL-29 - Create add medicines form in Medicines component

--- src/components/DaschboardPages/Medicines/Medicines.tsx
+++ src/components/DaschboardPages/Medicines/Medicines.tsx
@@ -1,11 +1,77 @@
 import React from 'react';
-import {StyledContainer, StyledAllContainer, StyledFirstContainer, StyledSecondContainer, StyledThirdContainer} from './Medicines.styled';
+import {StyledContainer, StyledAllContainer, StyledFirstContainer, StyledSecondContainer, StyledThirdContainer, StyledDateTime, StyledTextField} from './Medicines.styled';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+  } from '@material-ui/pickers';  
+import InputAdornment from '@material-ui/core/InputAdornment';
+import DateFnsUtils from '@date-io/date-fns';
 
 export const Medicines: React.FC = () => {
+    const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+        new Date(),
+      );
+
+      const handleDateChange = (date: Date | null) => {
+        setSelectedDate(date);
+      };
     return (
         <StyledContainer>
             <StyledAllContainer><h1>All</h1></StyledAllContainer>
-            <StyledFirstContainer><h4>First</h4></StyledFirstContainer>
+            <StyledFirstContainer>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <StyledDateTime>
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="analyses-date"
+              label="Data badania"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />        
+        <KeyboardTimePicker
+          margin="normal"
+          id="analyses-time"
+          label="Godzina badania"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}
+        />
+        </StyledDateTime>
+        </MuiPickersUtilsProvider>
+            
+               <StyledTextField
+                  required
+                  id="outlined-required"
+                  label="dawka lekarstwa"
+                  defaultValue=""
+                  variant="outlined"
+                  size="small"
+                  InputProps={{
+                    endAdornment: <InputAdornment position="start">ml</InputAdornment>,
+                  }}
+                  
+                />
+                <StyledTextField
+                  required
+                  id="outlined-required"
+                  label="nazwa lekarstwa"
+                  defaultValue=""
+                  variant="outlined"
+                  size="small"
+                  InputProps={{
+                    endAdornment: <InputAdornment position="start">ml</InputAdornment>,
+                  }}
+                />
+            </StyledFirstContainer>
             <StyledSecondContainer><h4>Second</h4></StyledSecondContainer>
             <StyledThirdContainer><h4>Third</h4></StyledThirdContainer>
         </StyledContainer>
